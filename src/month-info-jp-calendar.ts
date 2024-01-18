a9aa844604a948e1e6bd0deccfb0c6af7853d0f5
kk3939 <kyo51310@gmail.com>
2024-01-18 21:44:56 +0900

    fix: initialize date

--- src/month-info-jp-calendar.ts
+++ src/month-info-jp-calendar.ts
@@ -6,14 +6,15 @@ export const monthInfoJpCalendar = (
   month: number,
   zone: string = "Asia/Tokyo",
 ) => {
-  const dt = DateTime.local(year, month).setZone(zone);
+  const dt = DateTime.fromObject({ year, month }, { zone });
   const numDays = dt.daysInMonth;
   const dates = [...Array(numDays).keys()].map((i) => {
     // index starts from 0
     // So, add 1 to the index to get the day of the month
-    const date = DateTime.local(year, month, i + 1)
-      .setZone(zone)
-      .toISO();
+    const date = DateTime.fromObject(
+      { year, month, day: i + 1 },
+      { zone },
+    ).toISO();
     return {
       date,
     };
