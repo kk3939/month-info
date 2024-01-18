95493a2e52538242ec5ba2ac9ba8324e8c2c9060
kk3939 <kyo51310@gmail.com>
2024-01-18 20:55:57 +0900

    add: numDays

--- src/month-info-jp-calendar.test.ts
+++ src/month-info-jp-calendar.test.ts
@@ -1,9 +1,13 @@
 import { monthInfoJpCalendar } from "./month-info-jp-calendar";
 
 // to test
-describe("test", () => {
-  it("test", () => {
-    const monthInfo = monthInfoJpCalendar();
+describe("numDays", () => {
+  it("2024/01", () => {
+    const monthInfo = monthInfoJpCalendar(2024, 1);
     expect(monthInfo.numDays).toBe(31);
+  });
+  it("2024/02", () => {
+    const monthInfo = monthInfoJpCalendar(2024, 2);
+    expect(monthInfo.numDays).toBe(29);
   });
 });
--- src/month-info-jp-calendar.ts
+++ src/month-info-jp-calendar.ts
@@ -1,5 +1,10 @@
-export const monthInfoJpCalendar = () => {
+import { DateTime } from "luxon";
+
+// Return the number of days in a month
+export const monthInfoJpCalendar = (year: number, month: number) => {
+  const dt = DateTime.local(year, month);
+  const numDays = dt.daysInMonth;
   return {
-    numDays: 31,
+    numDays,
   };
 };
