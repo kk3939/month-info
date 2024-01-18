c499aace045b82251a1ccc214317a3c066fbc07a
kk3939 <kyo51310@gmail.com>
2024-01-18 21:28:31 +0900

    feat: get dates based on argument

--- src/month-info-jp-calendar.test.ts
+++ src/month-info-jp-calendar.test.ts
@@ -1,6 +1,5 @@
 import { monthInfoJpCalendar } from "./month-info-jp-calendar";
 
-// to test
 describe("numDays", () => {
   it("2024/01", () => {
     const monthInfo = monthInfoJpCalendar(2024, 1);
@@ -9,5 +8,22 @@ describe("numDays", () => {
   it("2024/02", () => {
     const monthInfo = monthInfoJpCalendar(2024, 2);
     expect(monthInfo.numDays).toBe(29);
+  });
+});
+
+describe("dates", () => {
+  it("2024/01 has the same length of the days of the month", () => {
+    const monthInfo = monthInfoJpCalendar(2024, 1);
+    expect(monthInfo.dates.length).toBe(31);
+  });
+  it("2024/02 has the same length of the days of the month", () => {
+    const monthInfo = monthInfoJpCalendar(2024, 2);
+    expect(monthInfo.dates.length).toBe(29);
+  });
+  it("2024/01/01", () => {
+    const monthInfo = monthInfoJpCalendar(2024, 1);
+    console.log(monthInfo);
+
+    expect(monthInfo.dates[0].date).toBe("2024-01-01T00:00:00.000+09:00");
   });
 });
--- src/month-info-jp-calendar.ts
+++ src/month-info-jp-calendar.ts
@@ -1,10 +1,26 @@
 import { DateTime } from "luxon";
 
 // Return the number of days in a month
-export const monthInfoJpCalendar = (year: number, month: number) => {
-  const dt = DateTime.local(year, month);
+export const monthInfoJpCalendar = (
+  year: number,
+  month: number,
+  zone: string = "Asia/Tokyo",
+) => {
+  const dt = DateTime.local(year, month).setZone(zone);
   const numDays = dt.daysInMonth;
+  const dates = [...Array(numDays).keys()].map((i) => {
+    // index starts from 0
+    // So, add 1 to the index to get the day of the month
+    const date = DateTime.local(year, month, i + 1)
+      .setZone(zone)
+      .toISO();
+    return {
+      date,
+    };
+  });
+
   return {
     numDays,
+    dates,
   };
 };
